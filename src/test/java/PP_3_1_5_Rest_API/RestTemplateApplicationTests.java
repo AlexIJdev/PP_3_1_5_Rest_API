bd0abd32495c6ebeb14083efc5a0cbe6d86d1557
Alexandr19961996 <98707400+Alexandr19961996@users.noreply.github.com>
2025-04-01 21:14:07 +0300

    Init commit

---
+++ src/test/java/PP_3_1_5_Rest_API/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package PP_3_1_5_Rest_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
